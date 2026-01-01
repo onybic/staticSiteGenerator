d75e1ef3ffb1c445cf9c83bc03f4b3ef7a1d9ee0
onybic <onybic@gmail.com>
2026-01-01 17:58:07 +0100

    publish

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py https://onybic.github.io/staticSiteGenerator
--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
-dir_path_static = "./static"
-dir_path_public = "./public"
-dir_path_content = "./content"
-template_path = "./template.html"
+dir_path_static: str = "./static"
+dir_path_public: str = "./docs"
+dir_path_content: str = "./content"
+template_path: str = "./template.html"
+default_basepath: str = "/"
 
 
 def main():
+
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -20,7 +27,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
